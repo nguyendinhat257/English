02c13ef95a0a795004c72d50856679240a9182bd
= <nhatnguyen2572001123@gmail.com>
2021-05-14 19:03:01 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-// var listChars = document.querySelectorAll(".char");
-// console.log(listChars);
-// var i = 0;
-//  var goback = setInterval( function(){
-//         console.log(i);
-//         listChars[i].classList.add("animate");
-//         i++;
-//         if (i == listChars.length) {
-//             clearInterval(goback);
-//             let title = document.querySelector(".title");
-//            setTimeout(()=>{
-//             title.style.display = "none";
-//             title = document.querySelector(".container");
-//             title.classList.add("show");
-//            },2000)
-//         }
-//     },200);
+var listChars = document.querySelectorAll(".char");
+console.log(listChars);
+var i = 0;
+ var goback = setInterval( function(){
+        console.log(i);
+        listChars[i].classList.add("animate");
+        i++;
+        if (i == listChars.length) {
+            clearInterval(goback);
+            let title = document.querySelector(".title");
+           setTimeout(()=>{
+            title.style.display = "none";
+            title = document.querySelector(".container");
+            title.classList.add("show");
+           },2000)
+        }
+    },200);
 
 var listParas = document.querySelectorAll("p");
 console.log(listParas);
